88f4811a0c09a244f5749d71d94d32bee50483d0
Larissa Pissurno <larissapissurno@gmail.com>
2025-03-09 15:08:07 -0300

    leticia says

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div style={styles.container}>
-      Hi 👋 I'm Larissa and this is my piece of internet.
+      <div style={styles.title}>
+        Hi! 👋 I'm Larissa and this is my piece of internet.
+      </div>
+
+      <div style={styles.content}>
+        <p>👩‍⚕️ Letícia Pissurno says: </p>
+        <img src="/sis.jpg" width={200} alt="My sis loves me" />
+      </div>
     </div>
   );
 }
@@ -9,11 +16,34 @@ export default function Home() {
 const styles = {
   container: {
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     height: "100vh",
+    gap: 48,
 
     fontFamily: "sans-serif",
+    fontSize: 16,
+  },
+
+  img: {
+    width: 200,
+    height: 200,
+  },
+
+  title: {
     fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 48,
+  },
+
+  content: {
+    display: "flex",
+    alignItems: "center",
+    gap: 24,
+
+    // border: "1px solid black",
+
+    width: "50vw",
   },
 };
